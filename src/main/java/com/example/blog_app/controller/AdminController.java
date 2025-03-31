d5fd196ab9eba633b659b9e12d1ee4d33810aaab
baoacer <nguyenquocbaotu@gmail.com>
2025-04-01 01:02:14 +0700

    convert thymeleaf

--- src/main/java/com/example/blog_app/controller/AdminController.java
+++ src/main/java/com/example/blog_app/controller/AdminController.java
@@ -4,8 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
- @GetMapping("/admin")
+    @GetMapping("/admin")
     public String adminPage() {
         return "admin"; // Trả về file admin.html trong thư mục templates
+    }
+
+    @GetMapping("/home")
+    public String homePage() {
+        return "index";
     }
 }
